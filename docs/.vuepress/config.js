ee4337e947d4ad101cceb9f7f48c93d9f5ca3a23
Jerry_sun <731238433@qq.com>
2020-11-06 13:36:39 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   title: 'Jerry的博客',
   description: '专注于前后端技术反向，从前端到Node.js/Java再到数据库',
+  dest: './dist'
   head: [
     ['link', { rel: 'icon', href: `/favicon.ico` }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
